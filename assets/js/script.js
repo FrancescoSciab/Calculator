114d4c73b27914ad8ed2fc8763296227d09ff321
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-01-08 15:11:56 +0000

    version 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+//clear function
+    clear(){
+
+    }
+    
+//delete
+    delete(){
+
+    }
+
+    appendNumber(){
+
+    }
+
+    chooseOperation(){
+
+    }
+
+    compute(){
+
+    }
+
+    getDisplayNumber(){
+
+    }
+    
+    updateDisplay(){
+
+    }
+}
+    
+
 const numbers = document.querySelectorAll("[data-number]");
 const operationBtns = document.querySelectorAll("[data-operation]");
 const equalBtn = document.querySelector("[data-equals]");
