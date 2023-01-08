2dbd0e20567b6f226bc63903b33ec0f3f78eda24
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-01-08 14:24:06 +0000

    version 1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const numbers = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
